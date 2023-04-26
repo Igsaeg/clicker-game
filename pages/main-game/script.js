72dbdfd3aed11d839b27b19bafb0250f88fd3586
Igsaeg <kharlfranco26@gmail.com>
2023-04-26 18:55:28 +0800

    v2.5

--- pages/main-game/script.js
+++ pages/main-game/script.js
@@ -11,12 +11,21 @@ const upgradeTextElement = document.getElementById("upgradeText");
 let clicks = parseInt(localStorage.getItem('clicks')) || 0;
 let noOfClicks = parseInt(localStorage.getItem('noOfClicks')) || 1;
 let type = parseInt(localStorage.getItem('type')) || 1;
+let clickersNoOfClicks = parseInt(localStorage.getItem('clickersNoOfClicks')) || 5;
+let clickersStateGate = parseInt(localStorage.getItem('clickersStateGate')) || 0;
+let mainClickersUpgradeState = parseInt(localStorage.getItem('mainClickersUpgradeState')) || 0;
+let tenCounter = parseInt(localStorage.getItem('tenCounter')) || 0;
+let clickersState = 0
 
 // Functions
 function saveProgress() {
   localStorage.setItem('clicks', clicks);
   localStorage.setItem('noOfClicks', noOfClicks);
   localStorage.setItem('type', type);
+  localStorage.setItem('clickersNoOfClicks', clickersNoOfClicks);
+  localStorage.setItem('clickersStateGate', clickersStateGate);
+  localStorage.setItem('mainClickersUpgradeState', mainClickersUpgradeState);
+  localStorage.setItem('tenCounter', tenCounter);
 }
 
 function updateUpgradeText() {
@@ -30,7 +39,26 @@ function add() {
   saveProgress();
   const clickSound = new Audio("audio/click.mp3");
   clickSound.play();
+  if (clickersStateGate === 1) {
+    clickersState = 1;
+  }
 }
+
+function clickersAdd() {
+  clicks += clickersNoOfClicks;
+  clickCountElement.innerHTML = `Clicks: ${clicks}`;
+  saveProgress();
+  const clickSound = new Audio("audio/click.mp3");
+  clickSound.play();
+}
+
+function clickers() {
+  if (clickersState === 1) {
+    clickersAdd(); 
+  }
+}
+
+setInterval(clickers, 1000);
 
 function upgrade() {
   const nextUpgradeClicks = upgradeClicks[type];
@@ -38,19 +66,45 @@ function upgrade() {
     type++;
     clicks -= nextUpgradeClicks;
     noOfClicks = upgradeValues[type];
+    clickCountElement.innerHTML = `Clicks: ${clicks}`;
     currentPerClickElement.innerHTML = `${noOfClicks} per click`;
     saveProgress();
     updateUpgradeText();
-    clickCountElement.innerHTML = `Clicks: ${clicks}`;
+    clickersUpgradeChecker();
     const upgradeSound = new Audio("audio/upgrade.mp3");
     upgradeSound.play();
   }
+}
+
+function clickersUpgradeChecker() {
+  if (type === 10) {
+    clickersState = 1;
+    clickersStateGate = 1;
+    mainClickersUpgradeState = 1;
+    alert('Clickers have been added!')
+  }
+  if (mainClickersUpgradeState === 1) {
+    if (tenCounter === 10) {
+      clickersUpgrade();
+      tenCounter = 0;
+    }
+  tenCounter++;
+  }
+  saveProgress();
+}
+
+
+function clickersUpgrade() {
+  clickersNoOfClicks += 5;
 }
 
 function init() {
   updateUpgradeText();
   clickCountElement.innerHTML = `Clicks: ${clicks}`;
   currentPerClickElement.innerHTML = `${noOfClicks} per click`;
+  if (clickersStateGate === 1) {
+    alert('Reminder! Clickers wont start automatically. Press the click button to reactivate it!')
+  }
 }
 
 // Initialization
--- pages/update-info/script.js
+++ pages/update-info/script.js
@@ -3,6 +3,14 @@ function backToGame() {
   window.location.href = "index.html";
 }
 
+function anchorDown() {
+  document.querySelector("#down").scrollIntoView({behavior: "smooth"});
+}
+
+function anchorUp() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
 // Initialization
 document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
